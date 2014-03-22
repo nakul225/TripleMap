4750ee6e30ce10547ae03f0f10c7536eaf25b2aa
nakul225 <nakul@linuxmint.linuxmint-domain>
2014-03-22 02:44:36 -0400

    Added flask files to be pushed to Google App engine

---
+++ web/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, current_app, request, session, Flask, render_template, \
+    flash, send_from_directory, redirect, g
+
+app = Flask(__name__)
+app.debug = True
+app.config.from_object(__name__)
+
+
+@app.route('/')
+def load():
+	print "Loading!!"
+	return render_template('mapTest.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    """Return a custom 500 error."""
+    print 
+    return 'Sorry, unexpected error:\n{}'.format(e), 500
+
+def main():
+	app.debug = True
+	app.run()
+
+if __name__ == '__main__':
+	main()
