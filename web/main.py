5c7e91d514bb1a65f26d4165799ee44290689d36
Pushkar Joshi <psjoshi@indiana.edu>
2014-03-22 04:38:22 -0400

    Rename server.py to main.py.

--- web/main.py
+++ web/main.py
@@ -1,15 +1,19 @@
 from flask import Flask, current_app, request, session, Flask, render_template, \
-    flash, send_from_directory, redirect, g
+        flash, send_from_directory, redirect, g
+from pprint import pprint
 
 app = Flask(__name__)
 app.debug = True
 app.config.from_object(__name__)
 
 
-@app.route('/')
+@app.route('/route', methods=['POST', 'GET'])
 def load():
-	print "Loading!!"
-	return render_template('mapTest.html')
+    print "Loading!!"
+    if request.method == "POST":
+        userRequestLat = request.form['lat']
+        userRequestLng = request.form['lng']
+    return render_template('mapTest.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -20,12 +24,12 @@ def page_not_found(e):
 @app.errorhandler(500)
 def server_error(e):
     """Return a custom 500 error."""
-    print 
+    print
     return 'Sorry, unexpected error:\n{}'.format(e), 500
 
 def main():
-	app.debug = True
-	app.run()
+    app.debug = True
+    app.run()
 
 if __name__ == '__main__':
-	main()
+    main()
