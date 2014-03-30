0369fa8b1c79e56621fc128c5736a02faf790466
Pushkar Joshi <psjoshi@indiana.edu>
2014-03-30 17:33:55 -0400

    Client changes.

--- web/static/mapTest.js
+++ web/static/mapTest.js
@@ -4,21 +4,26 @@ script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 function setMap(){
-  window.map;
+  window.map = "test";
 }
 
 function initialize() {
     var map_canvas = document.getElementById('map_canvas');
     var map_options = {
         center: new google.maps.LatLng(39.13858199058352, -86.5118408203125),
-        zoom: 12,
+        zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     window.map = new google.maps.Map(map_canvas, map_options);
 
     google.maps.event.addListener(map, 'click', function(event) {
         //Get the position of clicked point
-        $.post( "/", { lat:event.latLng.lat(), lng:event.latLng.lng()});
+        var formData = {  lat:event.latLng.lat(),
+                          lng:event.latLng.lng(),
+                          univ: $('#selectUniv').val(),
+                          busList: $('#')
+                        }
+        $.post("/", formData);
         //TODO: why doesnt the event 'dblclick' stop propogating! x(
         //That is because the zoom has been already triggered before this line is executed.
         event.stop();
@@ -31,11 +36,9 @@ function addMark(){
   alert($('#address').val());
 }
 
-
-function changeCity(univOptions, cities){
-  a = typeof(univOptions);
-  //var map_canvas = document.getElementById('map_canvas');
-  //window.alert(univOptions);
-  //window.map.setCenter(new google.maps.LatLng( 45, 19 ) );
+function changeCity(univOptions, coordinates){
+  latLng = coordinates.split(':');
+  window.map.setZoom(14);
+  window.map.setCenter(new google.maps.LatLng( latLng[0], latLng[1] ) );
 }
 
